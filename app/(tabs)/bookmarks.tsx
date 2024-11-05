3668eace9875058f7e4ba072e06fb99880c4f47e
IIRoan <82589897+IIRoan@users.noreply.github.com>
2024-11-05 17:43:52 +0100

    Refactored Code, Put Titles/navbar in safe area

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,16 +1,26 @@
 import React, { useCallback, useState, useEffect } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet, ActivityIndicator, SafeAreaView, StatusBar } from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from 'expo-router';
 import { useFocusEffect } from '@react-navigation/native';
 import { useTheme } from '@/constants/ThemeContext';
-import { Colors } from '@/constants/Colors';
+import { Colors, ColorScheme } from '@/constants/Colors';
 import { Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import MangaCard from '@/components/MangaCard';
 import { getLastReadChapter } from '@/services/readChapterService';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-// Types
+/* Type Definitions */
 interface BookmarkItem {
   id: string;
   title: string;
@@ -22,27 +32,37 @@ interface BookmarkItem {
 export default function BookmarksScreen() {
   const [bookmarks, setBookmarks] = useState<BookmarkItem[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [activeSection, setActiveSection] = useState('Reading');
+  const [activeSection, setActiveSection] = useState<'Reading' | 'To Read' | 'Read'>('Reading');
   const router = useRouter();
+
+  // Theme and styling
   const { actualTheme } = useTheme();
   const colors = Colors[actualTheme];
   const styles = getStyles(colors);
 
+  // Safe area insets
+  const insets = useSafeAreaInsets();
+
+  // Fetch bookmarks from AsyncStorage
   const fetchBookmarks = useCallback(async () => {
     setIsLoading(true);
     try {
       const keys = await AsyncStorage.getItem('bookmarkKeys');
       const bookmarkKeys = keys ? JSON.parse(keys) : [];
       const bookmarkPromises = bookmarkKeys.map(async (key: string) => {
-        const status = await AsyncStorage.getItem(key);
+        const status = (await AsyncStorage.getItem(key)) || '';
         const id = key.split('_')[1];
-        const title = await AsyncStorage.getItem(`title_${id}`);
+        const title = (await AsyncStorage.getItem(`title_${id}`)) || '';
         const lastReadChapter = await getLastReadChapter(id);
-        const imageUrl = await AsyncStorage.getItem(`image_${id}`);
+        const imageUrl = (await AsyncStorage.getItem(`image_${id}`)) || '';
         return { id, title, status, lastReadChapter, imageUrl };
       });
       const bookmarkItems = await Promise.all(bookmarkPromises);
-      setBookmarks(bookmarkItems.filter((item): item is BookmarkItem => item.title !== null && item.imageUrl !== null));
+      setBookmarks(
+        bookmarkItems.filter(
+          (item): item is BookmarkItem => item.title !== '' && item.imageUrl !== ''
+        )
+      );
     } catch (error) {
       console.error('Error fetching bookmarks:', error);
     } finally {
@@ -89,7 +109,7 @@ export default function BookmarksScreen() {
     </View>
   );
 
-  const renderSectionButton = (title: string) => {
+  const renderSectionButton = (title: 'Reading' | 'To Read' | 'Read') => {
     let iconName: keyof typeof Ionicons.glyphMap;
     switch (title) {
       case 'To Read':
@@ -107,7 +127,10 @@ export default function BookmarksScreen() {
 
     return (
       <TouchableOpacity
-        style={[styles.sectionButton, activeSection === title && styles.activeSectionButton]}
+        style={[
+          styles.sectionButton,
+          activeSection === title && styles.activeSectionButton,
+        ]}
         onPress={() => setActiveSection(title)}
       >
         <Ionicons
@@ -116,10 +139,12 @@ export default function BookmarksScreen() {
           color={activeSection === title ? colors.card : colors.text}
           style={styles.sectionButtonIcon}
         />
-        <Text style={[
-          styles.sectionButtonText,
-          activeSection === title && styles.activeSectionButtonText
-        ]}>
+        <Text
+          style={[
+            styles.sectionButtonText,
+            activeSection === title && styles.activeSectionButtonText,
+          ]}
+        >
           {title}
         </Text>
       </TouchableOpacity>
@@ -135,8 +160,9 @@ export default function BookmarksScreen() {
       </View>
     );
   }
+
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, { paddingTop: insets.top }]}>
       <Text style={styles.header}>My Bookmarks</Text>
       <View style={styles.sectionButtonsContainer}>
         {renderSectionButton('Reading')}
@@ -160,90 +186,72 @@ export default function BookmarksScreen() {
   );
 }
 
-const getStyles = (colors: typeof Colors.light) => StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.background,
-    paddingTop: StatusBar.currentHeight || 0, // Ensure padding accounts for the status bar
-  },
-  header: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: colors.text,
-    paddingHorizontal: 20,
-    marginTop: 20 - (StatusBar.currentHeight || 0), // Ensure 30 pixels from the top
-    paddingBottom: 10,
-  },
-  sectionButtonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 20,
-    marginBottom: 20,
-  },
-  sectionButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderRadius: 20,
-    backgroundColor: colors.card,
-  },
-  activeSectionButton: {
-    backgroundColor: colors.primary,
-  },
-  sectionButtonIcon: {
-    marginRight: 8,
-  },
-  sectionButtonText: {
-    fontSize: 14,
-    fontWeight: '600',
-    color: colors.text,
-  },
-  activeSectionButtonText: {
-    color: colors.card,
-  },
-  contentContainer: {
-    paddingHorizontal: 15,
-    paddingBottom: 80,
-  },
-  columnWrapper: {
-    justifyContent: 'space-between',
-  },
-  bookmarkCardWrapper: {
-    width: '48%',
-    marginBottom: 15,
-  },
-  bookmarkCard: {
-    flex: 1,
-  },
-  loadingContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.background,
-  },
-  emptyText: {
-    fontSize: 18,
-    textAlign: 'center',
-    marginTop: 40,
-    color: colors.text,
-  },
-  bookmarkImage: {
-    width: '100%',
-    height: 200,
-    resizeMode: 'cover',
-  },
-  bookmarkInfo: {
-    padding: 10,
-  },
-  bookmarkTitle: {
-    fontSize: 14,
-    fontWeight: '600',
-    color: colors.text,
-    marginBottom: 4,
-  },
-  lastReadChapter: {
-    fontSize: 12,
-    color: colors.tabIconDefault,
-  },
-});
+const getStyles = (colors: typeof Colors.light) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+    },
+    header: {
+      fontSize: 28,
+      fontWeight: 'bold',
+      color: colors.text,
+      paddingHorizontal: 20,
+      paddingBottom: 10,
+    },
+    sectionButtonsContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingHorizontal: 20,
+      marginBottom: 20,
+    },
+    sectionButton: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingVertical: 8,
+      paddingHorizontal: 16,
+      borderRadius: 20,
+      backgroundColor: colors.card,
+    },
+    activeSectionButton: {
+      backgroundColor: colors.primary,
+    },
+    sectionButtonIcon: {
+      marginRight: 8,
+    },
+    sectionButtonText: {
+      fontSize: 14,
+      fontWeight: '600',
+      color: colors.text,
+    },
+    activeSectionButtonText: {
+      color: colors.card,
+    },
+    contentContainer: {
+      paddingHorizontal: 15,
+      paddingBottom: 80,
+    },
+    columnWrapper: {
+      justifyContent: 'space-between',
+    },
+    bookmarkCardWrapper: {
+      width: '48%',
+      marginBottom: 15,
+    },
+    bookmarkCard: {
+      flex: 1,
+    },
+    loadingContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: colors.background,
+    },
+    emptyText: {
+      fontSize: 18,
+      textAlign: 'center',
+      marginTop: 40,
+      color: colors.text,
+    },
+  });
+
