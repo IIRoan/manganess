0182c5b9624fee0d686996db7bb097262c56e774
IIRoan <82589897+IIRoan@users.noreply.github.com>
2025-02-24 23:57:22 +0100

    Code Cleanup moved types to own file

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+// This file exports all types for easier imports
+
+export * from './manga';
+export * from './auth';
+export * from './navigation';
+export * from './settings';
+export * from './ui';
+export * from './api';
