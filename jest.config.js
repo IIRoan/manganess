121d6d4e6f591eeaa280e2df9d48147c73f79ec8
IIRoan <github.com.q6c0y@lunary.roan.zip>
2026-03-03 22:37:04 +0100

    test: configure Jest mocks for binary assets and Expo modules

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,9 @@ module.exports = {
   moduleNameMapper: {
     ...(preset.moduleNameMapper || {}),
     '^@/(.*)$': '<rootDir>/$1',
+    // Mock binary assets
+    '\\.(ttf|otf|eot|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|svg|webp)$': '<rootDir>/__mocks__/fileMock.js',
   },
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
 
